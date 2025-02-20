c498e8cc520c29bcd6bbb9fc89148695540f23f3
Md. Nahid Hasan <nahidhasan.dev1@gmail.com>
2025-02-20 20:42:12 +0600

    my changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import "./globals.css";
 import LeftBar from './../components/LeftBar';
 import RightBar from './../components/RightBar';
 import NavBar from "@/components/NavBar";
+import Head from "next/head";
 
 
 export const metadata = {
@@ -12,6 +13,28 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <meta name="title" content="Huda Connection" />
+        <meta name="description" content="A platform to connect and share Islamic knowledge with the world." />
+        <meta name="keywords" content="Islam, Muslim Community, Quran, Hadith, Islamic Knowledge, Religion, Mahfil" />
+        <meta name="author" content="Huda Connection Team" />
+        <meta name="robots" content="index, follow" />
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Huda Connection" />
+        <meta property="og:description" content="A platform to connect and share Islamic knowledge with the world." />
+        <meta property="og:image" content="/huda-connection-banner.jpg" />
+        <meta property="og:url" content="https://hudaconnection.com" />
+        <meta property="og:site_name" content="Huda Connection" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Huda Connection" />
+        <meta name="twitter:description" content="A platform to connect and share Islamic knowledge with the world." />
+        <meta name="twitter:image" content="/huda-connection-banner.jpg" />
+        <meta name="twitter:site" content="@HudaConnection" />
+      </Head>
       <body className="">
         <div className="max-w-screen-xl lg:max-w-screen-lg xl:max-w-screen-xl xxl:max-w-screen-xxl mx-auto flex justify-between">
           <div className="hidden lg:flex px-2 xxl:px-8 mr-4 md:mr-8 bg-white rounded-lg border-x-[1px] border-gray-200"><LeftBar /></div>
